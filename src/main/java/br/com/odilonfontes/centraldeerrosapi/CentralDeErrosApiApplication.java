268d8a6aadcf09112346bf1a9dbb02ebe7b9ca7a
Odilon Fontes <odilonfontes@gmail.com>
2020-08-09 14:29:32 -0300

    Commit inicial

---
+++ src/main/java/br/com/odilonfontes/centraldeerrosapi/CentralDeErrosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.odilonfontes.centraldeerrosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralDeErrosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentralDeErrosApiApplication.class, args);
+    }
+
+}
