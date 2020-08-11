57d4fb01ddc6038f4cb828e6e872393efc6219dc
Odilon Fontes <odilonfontes@gmail.com>
2020-08-11 19:52:04 -0300

    Alteração do nome da classe CentralDeErrosApi para ErrorCenterApiApplication

--- src/main/java/br/com/odilonfontes/errorcenterapi/ErrorCenterApiApplication.java
+++ src/main/java/br/com/odilonfontes/errorcenterapi/ErrorCenterApiApplication.java
@@ -1,13 +1,13 @@
-package br.com.odilonfontes.centraldeerrosapi;
+package br.com.odilonfontes.errorcenterapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CentralDeErrosApiApplication {
+public class ErrorCenterApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CentralDeErrosApiApplication.class, args);
+        SpringApplication.run(ErrorCenterApiApplication.class, args);
     }
 
 }
