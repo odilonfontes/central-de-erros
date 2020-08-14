bff7f1a3d4ddbb0636df075e4887ee2e95f2b3e3
Odilon Fontes <odilonfontes@gmail.com>
2020-08-13 23:03:34 -0300

    Renomeada de User para Event

---
+++ src/main/java/br/com/odilonfontes/errorcenterapi/domain/Event.java
@@ -0,0 +1,56 @@
+package br.com.odilonfontes.errorcenterapi.domain;
+
+import br.com.odilonfontes.errorcenterapi.domain.enumeration.EventLevel;
+import lombok.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Event implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(value = AccessLevel.NONE)
+    private Long id;
+
+    @Size(max = 256)
+    @NotNull
+    private String description;
+
+    @Size(max = 32767)
+    @NotNull
+    private String log;
+
+    @Size(max = 256)
+    @NotNull
+    private String source;
+
+    @NotNull
+    @CreatedDate
+    @Setter(value = AccessLevel.NONE)
+    private LocalDateTime createdAt;
+
+    @NotNull
+    private EventLevel level;
+
+    @Column(name = "number_occurences")
+    @NotNull
+    private Integer occurences;
+
+}
