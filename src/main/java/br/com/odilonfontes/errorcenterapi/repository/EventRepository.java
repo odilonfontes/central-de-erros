9bf255c23e6a9fb57681aa84f6f95d07bf5c42a2
Odilon Fontes <odilonfontes@gmail.com>
2020-08-13 23:04:15 -0300

    Renomeada de UserRepository para EventRepository

--- src/main/java/br/com/odilonfontes/errorcenterapi/repository/EventRepository.java
+++ src/main/java/br/com/odilonfontes/errorcenterapi/repository/EventRepository.java
@@ -1,8 +1,8 @@
 package br.com.odilonfontes.errorcenterapi.repository;
 
-import br.com.odilonfontes.errorcenterapi.domain.User;
+import br.com.odilonfontes.errorcenterapi.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface EventRepository extends JpaRepository<Event, Long> {}
