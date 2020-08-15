94b8f9f083facc744202cb3dc200273c3c80d413
Odilon Fontes <odilonfontes@gmail.com>
2020-08-15 01:36:11 -0300

    Refatorada

--- src/main/java/br/com/odilonfontes/errorcenterapi/service/EventService.java
+++ src/main/java/br/com/odilonfontes/errorcenterapi/service/EventService.java
@@ -1,12 +1,12 @@
 package br.com.odilonfontes.errorcenterapi.service;
 
-import br.com.odilonfontes.errorcenterapi.domain.Event;
+import br.com.odilonfontes.errorcenterapi.service.dto.EventDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface EventService {
 
-    Event save(Event event);
-    Page<Event> findAll(Pageable pageable);
+    EventDTO save(EventDTO eventDTO);
+    Page<EventDTO> findAll(Pageable pageable);
 
 }
