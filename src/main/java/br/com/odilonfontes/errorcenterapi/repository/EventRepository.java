322101bdcb84c6692f35dc5fe1db03a6b0730504
Odilon Fontes <odilonfontes@gmail.com>
2020-08-16 23:02:09 -0300

    Criação do método "findByDescriptionAndSourceAndEventDateAndLevel"

--- src/main/java/br/com/odilonfontes/errorcenterapi/repository/EventRepository.java
+++ src/main/java/br/com/odilonfontes/errorcenterapi/repository/EventRepository.java
@@ -1,8 +1,19 @@
 package br.com.odilonfontes.errorcenterapi.repository;
 
 import br.com.odilonfontes.errorcenterapi.domain.Event;
+import br.com.odilonfontes.errorcenterapi.domain.enumeration.EventLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {}
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    Optional<Event> findByDescriptionAndSourceAndEventDateAndLevel(String description,
+                                                                   String source,
+                                                                   LocalDate eventDate,
+                                                                   EventLevel level);
+
+}
