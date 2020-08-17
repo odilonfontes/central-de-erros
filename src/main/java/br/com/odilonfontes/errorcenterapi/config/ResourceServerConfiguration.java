13b2aed61d92bc3928269138e48182d999ab6f92
Odilon Fontes <odilonfontes@gmail.com>
2020-08-16 23:00:26 -0300

    Alteração para permitir acesso sem autenticação a url "/h2"

--- src/main/java/br/com/odilonfontes/errorcenterapi/config/ResourceServerConfiguration.java
+++ src/main/java/br/com/odilonfontes/errorcenterapi/config/ResourceServerConfiguration.java
@@ -1,7 +1,6 @@
 package br.com.odilonfontes.errorcenterapi.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -13,7 +12,7 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/api/users/**").permitAll()
+                .antMatchers("/h2/**").permitAll()
                 .antMatchers("/api/**").authenticated();
     }
 
