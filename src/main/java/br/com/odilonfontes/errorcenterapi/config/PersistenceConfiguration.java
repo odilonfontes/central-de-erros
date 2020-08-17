82b856cacbf57fabc7b04b6f6af57db62340104a
Odilon Fontes <odilonfontes@gmail.com>
2020-08-16 22:56:17 -0300

    Inclusão do método exceptionTranslation

--- src/main/java/br/com/odilonfontes/errorcenterapi/config/PersistenceConfiguration.java
+++ src/main/java/br/com/odilonfontes/errorcenterapi/config/PersistenceConfiguration.java
@@ -1,8 +1,17 @@
 package br.com.odilonfontes.errorcenterapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
-public class PersistenceConfiguration {}
+public class PersistenceConfiguration {
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+}
