cd513fef2eb7e04b028a972b10025447989ba7ca
Odilon Fontes <odilonfontes@gmail.com>
2020-08-17 22:56:36 -0300

    Inclusão do endpoint "/events/{id}"

--- src/main/java/br/com/odilonfontes/errorcenterapi/web/rest/EventResource.java
+++ src/main/java/br/com/odilonfontes/errorcenterapi/web/rest/EventResource.java
@@ -4,6 +4,7 @@ import br.com.odilonfontes.errorcenterapi.service.EventService;
 import br.com.odilonfontes.errorcenterapi.service.dto.EventDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,9 +29,16 @@ public class EventResource {
     }
 
     @GetMapping("/events")
-    public ResponseEntity<List<EventDTO>> getAllEvents(Pageable pageable) {
+    public ResponseEntity<List<EventDTO>> getEvents(@PageableDefault(size = 50) Pageable pageable,
+                                                    @RequestParam(required = false) String description) {
         Page<EventDTO> result = eventService.findAll(pageable);
         return new ResponseEntity<>(result.getContent(), HttpStatus.OK);
+    }
+
+    @GetMapping("/events/{id}")
+    public ResponseEntity<EventDTO> getEvent(@PathVariable Long id) {
+        EventDTO result = eventService.findById(id);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
