86f6b43de4d1feaa26ab4fb95cd3fec8f9f2bc83
Odilon Fontes <odilonfontes@gmail.com>
2020-08-23 00:17:05 -0300

    Renomeada

--- src/main/java/br/com/odilonfontes/errorcenterapi/service/dto/LogEventInfoDTO.java
+++ src/main/java/br/com/odilonfontes/errorcenterapi/service/dto/LogEventInfoDTO.java
@@ -1,35 +1,30 @@
 package br.com.odilonfontes.errorcenterapi.service.dto;
 
-import br.com.odilonfontes.errorcenterapi.domain.enumeration.EventLevel;
+import br.com.odilonfontes.errorcenterapi.domain.enumeration.LogEventLevel;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class EventDTO implements Serializable {
+public class LogEventInfoDTO implements Serializable {
 
     private Long id;
 
-    @NotNull
     private String description;
 
-    @NotNull
     private String log;
 
-    @NotNull
     private String source;
 
     private String eventDate;
 
-    @NotNull
-    private EventLevel level;
+    private LogEventLevel level;
 
     private Integer quantity;
 
