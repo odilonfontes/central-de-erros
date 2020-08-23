5fa71bf5b7b67fa29e5fd90c6281dccd098de96c
Odilon Fontes <odilonfontes@gmail.com>
2020-08-23 00:17:22 -0300

    Renomeada

--- src/main/java/br/com/odilonfontes/errorcenterapi/service/specification/LogEventSpecification.java
+++ src/main/java/br/com/odilonfontes/errorcenterapi/service/specification/LogEventSpecification.java
@@ -1,46 +1,46 @@
 package br.com.odilonfontes.errorcenterapi.service.specification;
 
-import br.com.odilonfontes.errorcenterapi.domain.Event;
-import br.com.odilonfontes.errorcenterapi.domain.enumeration.EventLevel;
+import br.com.odilonfontes.errorcenterapi.domain.LogEvent;
+import br.com.odilonfontes.errorcenterapi.domain.enumeration.LogEventLevel;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.text.MessageFormat;
 import java.time.LocalDate;
 
-public final class EventSpecification {
+public final class LogEventSpecification {
 
-    public static Specification<Event> descriptionContains(String value) {
+    public static Specification<LogEvent> descriptionContains(String value) {
         return (value == null) ? null : (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder
                 .like(criteriaBuilder.lower(root.get("description")),
                         criteriaBuilder.lower(criteriaBuilder.literal(contains(value)))
                 );
     }
 
-    public static Specification<Event> logContains(String value) {
+    public static Specification<LogEvent> logContains(String value) {
         return (value == null) ? null : (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder
                 .like(criteriaBuilder.lower(root.get("log")),
                         criteriaBuilder.lower(criteriaBuilder.literal(contains(value)))
                 );
     }
 
-    public static Specification<Event> sourceContains(String value) {
+    public static Specification<LogEvent> sourceContains(String value) {
         return (value == null) ? null : (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder
                 .like(criteriaBuilder.lower(root.get("source")),
                         criteriaBuilder.lower(criteriaBuilder.literal(contains(value)))
                 );
     }
 
-    public static Specification<Event> eventDateEqual(LocalDate value) {
+    public static Specification<LogEvent> eventDateEqual(LocalDate value) {
         return (value == null) ? null : (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder
                 .equal(root.get("eventDate"), value);
     }
 
-    public static Specification<Event> levelEqual(EventLevel value) {
+    public static Specification<LogEvent> levelEqual(LogEventLevel value) {
         return (value == null) ? null : (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder
                 .equal(root.get("level"), value);
     }
 
-    public static Specification<Event> quantityEqual(Integer value) {
+    public static Specification<LogEvent> quantityEqual(Integer value) {
         return (value == null) ? null : (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder
                 .equal(root.get("quantity"), value);
     }
